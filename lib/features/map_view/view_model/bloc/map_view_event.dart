46f90edabd77b5eb75751339269921e2de3d05aa
BilalQanoa <bilalqanoa@gmail.com>
2025-06-22 09:50:25 +0300

    prepare map view states and map view events

--- lib/features/map_view/view_model/bloc/map_view_event.dart
+++ lib/features/map_view/view_model/bloc/map_view_event.dart
@@ -2,4 +2,27 @@ part of 'map_view_bloc.dart';
 
 sealed class MapViewEvent extends Equatable {
   const MapViewEvent();
+
+  @override
+  List<Object> get props => [];
 }
+
+class LoadMapEvents extends MapViewEvent {}
+
+class FilterMapEvents extends MapViewEvent {
+  final String category;
+  const FilterMapEvents(this.category);
+
+  @override
+  List<Object> get props => [category];
+}
+
+class SelectMarkerEvent extends MapViewEvent {
+  final MapModel selectedEvent;
+  const SelectMarkerEvent(this.selectedEvent);
+
+  @override
+  List<Object> get props => [selectedEvent];
+}
+
+class ClearFilters extends MapViewEvent {}
--- lib/features/map_view/view_model/bloc/map_view_state.dart
+++ lib/features/map_view/view_model/bloc/map_view_state.dart
@@ -2,9 +2,44 @@ part of 'map_view_bloc.dart';
 
 sealed class MapViewState extends Equatable {
   const MapViewState();
-}
 
-final class MapViewInitial extends MapViewState {
   @override
   List<Object> get props => [];
+}
+
+final class MapInitial extends MapViewState {}
+
+class MapLoading extends MapViewState {}
+
+class MapSuccess extends MapViewState {
+  final List<MapModel> events;
+  const MapSuccess(this.events);
+
+  @override
+  List<Object> get props => [events];
+}
+
+class MapFailure extends MapViewState {
+  final String error;
+  const MapFailure(this.error);
+
+  @override
+  List<Object> get props => [error];
+}
+
+class MapFiltered extends MapViewState {
+  final List<MapModel> filteredEvents;
+  final String category;
+  const MapFiltered(this.filteredEvents, this.category);
+
+  @override
+  List<Object> get props => [filteredEvents, category];
+}
+
+class MapMarkerSelected extends MapViewState {
+  final MapModel selectedEvent;
+  const MapMarkerSelected(this.selectedEvent);
+
+  @override
+  List<Object> get props => [selectedEvent];
 }
