939a94a04c92c67b68c703bdda004afad6ca4176
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2025-07-05 16:35:56 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:event_booking_app/features/auth/view/register_view.dart';
 import 'package:event_booking_app/features/splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const RegisterView(),
+      home: const SplashView(),
     );
   }
 }
