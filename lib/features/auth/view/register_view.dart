df924b8df723bc0cfcd693a4ba891a17f2d1c368
MohammedYasserBogyMan <158835899+MohammedYasserBogyMan@users.noreply.github.com>
2025-07-05 16:45:24 +0300

    final test

--- lib/features/auth/view/register_view.dart
+++ lib/features/auth/view/register_view.dart
@@ -1,5 +1,3 @@
-import 'package:event_booking_app/core/widgets/custom_button.dart';
-import 'package:event_booking_app/core/widgets/custom_text_filed.dart';
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatelessWidget {
@@ -7,22 +5,6 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          CustomTextFiled(icon: Icons.person_outline, hintText: "Full name"),
-          SizedBox(height: 19),
-          CustomTextFiled(icon: Icons.mail_outline, hintText: "abc@email.com"),
-          SizedBox(height: 19),
-          CustomTextFiled(icon: Icons.lock_outline, hintText: "Your password"),
-          SizedBox(height: 19),
-          CustomTextFiled(icon: Icons.lock_outline, hintText: "Confirm password"),
-          SizedBox(height: 19),
-          CustomTextFiled(icon: Icons.lock_outline, hintText: "Confirm password"),
-          SizedBox(height: 19),
-          CustomButton(text: "SIGN UP")
-        ],
-      ),
-    );
+    return Scaffold(body: Column(children: [Text('توكلنا علي الله')]));
   }
 }
