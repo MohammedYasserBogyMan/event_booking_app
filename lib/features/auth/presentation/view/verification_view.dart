21f5f3ee954c3d9f565b991b76760244f5352d84
abdallah <amabdgalil@gmail.com>
2025-07-06 15:21:59 +0300

    create Verification view

---
+++ lib/features/auth/presentation/view/verification_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VerificationView extends StatelessWidget {
+  const VerificationView({super.key});
+  static String id = "VerificationView";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:event_booking_app/features/auth/presentation/view/login_view.dart';
 import 'package:event_booking_app/features/auth/presentation/view/register_view.dart';
+import 'package:event_booking_app/features/auth/presentation/view/verification_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       routes: {
         RegisterView.id: (context) => RegisterView(),
         LoginView.id: (context) => LoginView(),
+        VerificationView.id: (context) => VerificationView(),
       },
       debugShowCheckedModeBanner: false,
       home: LoginView(),
