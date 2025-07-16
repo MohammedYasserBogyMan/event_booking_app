e60e1d281108152d39b2b133f83a660d333d505e
abdallah <amabdgalil@gmail.com>
2025-07-16 23:02:07 +0300

    refactor drawer menu list to use model-based dynamic rendering

--- lib/features/home/presentation/view/widgets/drawer_menu_item.dart
+++ lib/features/home/presentation/view/widgets/drawer_menu_item.dart
@@ -1,18 +1,18 @@
 import 'package:event_booking_app/core/constants/app_color.dart';
 import 'package:event_booking_app/core/utils/styels.dart';
+import 'package:event_booking_app/features/home/data/model/drawer_menu_item_model.dart';
 import 'package:flutter/material.dart';
 
 class DrawerMenuItem extends StatelessWidget {
-  const DrawerMenuItem({super.key, required this.text, required this.icon});
-  final String text;
-  final IconData icon;
+  const DrawerMenuItem({super.key, required this.drawerMenuItemModel});
+  final DrawerMenuItemModel drawerMenuItemModel;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(icon, color: AppColor.menuWhiteIconColor),
+        Icon(drawerMenuItemModel.icon, color: AppColor.menuWhiteIconColor),
         SizedBox(width: 10),
-        Text(text, style: Styels.textStyle16),
+        Text(drawerMenuItemModel.text, style: Styels.textStyle16),
       ],
     );
   }
--- lib/features/home/presentation/view/widgets/drawer_menu_list.dart
+++ lib/features/home/presentation/view/widgets/drawer_menu_list.dart
@@ -1,30 +1,36 @@
+import 'package:event_booking_app/features/home/data/model/drawer_menu_item_model.dart';
 import 'package:event_booking_app/features/home/presentation/view/widgets/drawer_menu_item.dart';
 import 'package:flutter/material.dart';
 
 class DrawerMenuList extends StatelessWidget {
-  const DrawerMenuList({super.key});
-
+  DrawerMenuList({super.key});
+  final List<DrawerMenuItemModel> menuList = [
+    DrawerMenuItemModel(
+      text: "My Profile",
+      icon: Icons.person_outline_outlined,
+    ),
+    DrawerMenuItemModel(text: "Message", icon: Icons.message_outlined),
+    DrawerMenuItemModel(text: "Calender", icon: Icons.calendar_today_outlined),
+    DrawerMenuItemModel(text: "Bookmark", icon: Icons.bookmark_outline),
+    DrawerMenuItemModel(text: "Contact Us", icon: Icons.mail_outline),
+    DrawerMenuItemModel(text: "Settings", icon: Icons.settings_outlined),
+    DrawerMenuItemModel(text: "Helps & FAQs", icon: Icons.help_outline),
+    DrawerMenuItemModel(text: "Sign Out", icon: Icons.search),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        DrawerMenuItem(text: "My Profile", icon: Icons.person_outline_outlined),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Massage", icon: Icons.message_outlined),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Calender", icon: Icons.calendar_today_outlined),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Bookmark", icon: Icons.bookmark_outline),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Contact Us", icon: Icons.mail_outline),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Settings", icon: Icons.settings_outlined),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Helps & FAQs", icon: Icons.help_outline),
-        SizedBox(height: 33),
-        DrawerMenuItem(text: "Sign Out", icon: Icons.search),
-        SizedBox(height: 33),
-      ],
+      children:
+          menuList
+              .map(
+                (item) => Column(
+                  children: [
+                    DrawerMenuItem(drawerMenuItemModel: item),
+                    const SizedBox(height: 33),
+                  ],
+                ),
+              )
+              .toList(),
     );
   }
 }
