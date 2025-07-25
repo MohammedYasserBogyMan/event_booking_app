43fb4e94109d14cab76ab0578b885a59b912ff36
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2025-07-25 17:07:34 +0300

    Add new routes to the appRouter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -8,6 +8,9 @@ import 'package:event_booking_app/features/home/presentation/view/settings_view.
 import 'package:event_booking_app/features/home/presentation/view/sign_out_view.dart';
 import 'package:event_booking_app/features/my_profile/presentation/view/my_profile_view.dart';
 import 'package:event_booking_app/features/notification/presentation/view/empty_notification_view.dart';
+import 'package:event_booking_app/features/notification/presentation/view/notification_view.dart';
+import 'package:event_booking_app/features/search/presentation/view/search_view.dart';
+import 'package:event_booking_app/features/see_all_events/presentation/view/see_all_events_view.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:event_booking_app/features/auth/presentation/view/login_view.dart';
@@ -19,23 +22,36 @@ import 'package:event_booking_app/features/splash/presentation/view/splash_view.
 
 abstract class AppRouter {
   static const splash = '/';
-  static const kHomeView = '/home';
   static const kOnboarding = '/onboarding';
   static const kLogin = '/login';
   static const kRegister = '/register';
-  static const kforgetpassword = '/forgetpassword';
-  static const kEmptyNotification = '/empty_notification';
   static const kVerification = '/verification';
+  static const kforgetpassword = '/forgetpassword';
+  static const kHomeView = '/home';
+  static const kEmptyNotification = '/empty_notification';
+  static const kNotificationView = '/notification';
   static const kBookMarkView = '/bookmark';
   static const kCalenderView = '/calender';
   static const kContactUsView = '/contactus';
-  static const kHelpAndFaqsView = '/helpandfaqs';
+  static const kHelpAndFaqsView = '/help_and_faqs';
   static const kMassageView = '/massage';
   static const kSettingView = '/setting';
-  static const kSignOutView = '/signout';
-  static const kMyProfile = '/myprofile';
+  static const kSignOutView = '/sign_out';
+  static const kMyProfile = '/my_profile';
+  static const kSeeAllEvents = '/see_all_events';
+  static const kSearchView = '/search';
+  static const kOrganizerProfile = '/organizer_profile';
   static final router = GoRouter(
     routes: [
+      GoRoute(
+        path: kNotificationView,
+        builder: (context, state) => NotificationView(),
+      ),
+      GoRoute(path: kSearchView, builder: (context, state) => SearchView()),
+      GoRoute(
+        path: kSeeAllEvents,
+        builder: (context, state) => SeeAllEventsView(),
+      ),
       GoRoute(path: kMyProfile, builder: (context, state) => MyProfileView()),
       GoRoute(path: kBookMarkView, builder: (context, state) => BookmarkView()),
       GoRoute(path: kCalenderView, builder: (context, state) => CalenderView()),
