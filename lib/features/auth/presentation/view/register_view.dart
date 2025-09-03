2ca080bbc8742cf55eab7c2c6d9b1a135a664572
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2025-09-03 03:27:16 +0300

    feat(auth): add loading state handling in RegisterView with ModalProgressHUD

--- lib/features/auth/presentation/view/register_view.dart
+++ lib/features/auth/presentation/view/register_view.dart
@@ -1,12 +1,27 @@
+import 'package:event_booking_app/core/constants/app_color.dart';
+import 'package:event_booking_app/features/auth/presentation/manager/auth_cubit/auth_cubit.dart';
+import 'package:event_booking_app/features/auth/presentation/manager/auth_cubit/auth_states.dart';
 import 'package:event_booking_app/features/auth/presentation/view/widgets/register_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterView extends StatelessWidget {
   const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: _buildAppBar(), body: RegisterViewBody());
+    return BlocBuilder<AuthCubit, AuthStates>(
+      builder: (context, state) {
+        bool isLoading = state is LoadingRegisterState;
+        return ModalProgressHUD(
+          blur: 10,
+          progressIndicator: CircularProgressIndicator(color: AppColor.primary),
+          inAsyncCall: isLoading,
+          child: Scaffold(appBar: _buildAppBar(), body: RegisterViewBody()),
+        );
+      },
+    );
   }
 
   AppBar _buildAppBar() {
