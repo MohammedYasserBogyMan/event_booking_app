1cc9a1496f5a71bf6de2f54b88b6fa42f0a41ae1
Abdallah Mohamed <115114629+AMOHAME0@users.noreply.github.com>
2025-10-04 18:10:22 +0300

    feat(event): update CreateEventView layout and logic

--- lib/features/create_event/presentation/views/create_event_view.dart
+++ lib/features/create_event/presentation/views/create_event_view.dart
@@ -1,10 +1,9 @@
-import 'package:event_booking_app/core/constants/app_color.dart';
 import 'package:event_booking_app/core/utils/styels.dart';
+import 'package:event_booking_app/core/widgets/modal_circular_progress.dart';
 import 'package:event_booking_app/features/create_event/presentation/manager/create_event_cubit/create_event_cubit.dart';
 import 'package:event_booking_app/features/create_event/presentation/views/widgets/create_event_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class CreateEventView extends StatelessWidget {
   const CreateEventView({super.key});
@@ -14,9 +13,7 @@ class CreateEventView extends StatelessWidget {
     return BlocBuilder<CreateEventCubit, CreateEventState>(
       builder: (context, state) {
         bool isLoading = state is CreateEventLoading;
-        return ModalProgressHUD(
-          blur: 10,
-          progressIndicator: CircularProgressIndicator(color: AppColor.primary),
+        return ModalCircularProgress(
           inAsyncCall: isLoading,
           child: Scaffold(
             appBar: AppBar(
